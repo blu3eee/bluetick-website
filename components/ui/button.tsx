d04027096e062164765bc44349ad1a592882bc73
bluesimp1102 <jackismyname7@gmail.com>
2024-03-10 17:30:59 -0700

    button blue variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        blue: 'text-white bg-blue-500 hover:bg-blue-500/70',
       },
       size: {
         default: 'h-10 px-4 py-2',
