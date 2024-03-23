94011f2cdad5c3a8949c2142d32ccb0757a05b79
bluesimp1102 <jackismyname7@gmail.com>
2024-03-23 00:48:24 -0700

    #43 ticket feature demo

--- components/home/features/index.tsx
+++ components/home/features/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import WelcomeFeature from './welcome';
-// import TicketFeature from './ticket';
+import TicketFeature from './ticket';
 
 const FeaturesDemo = (): JSX.Element => {
   return (
-    <section className="w-full">
+    <section className="w-full px-4 md:px-6 lg:px-12">
       <WelcomeFeature />
-      {/* <TicketFeature /> */}
+      <TicketFeature />
     </section>
   );
 };
