c6f83d2433e81488158301dd9aef913e455b6a11
bluesimp1102 <jackismyname7@gmail.com>
2024-03-23 00:47:06 -0700

    #43 add users' countries

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import HeroSection from '@/components/home/hero-section';
 import React from 'react';
 import BluetickIsAll from '@/components/home/bluetick-is-all';
 import FeaturesDemo from '@/components/home/features';
+import BotCountries from '@/components/home/bots-analytics/countries';
 
 const Page = (): JSX.Element => {
   return (
     <div>
       <HeroSection />
       <BluetickIsAll />
+      <BotCountries />
       <FeaturesDemo />
     </div>
   );
---
+++ components/home/bots-analytics/countries.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Skeleton } from '@/components/ui/skeleton';
+import { useFetchVercelDataCategory } from '@/hooks/vercel/insights';
+import { flag } from 'country-emoji';
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { poppinsFont } from '@/styles/fonts';
+
+const BotCountries = (): JSX.Element => {
+  const today = new Date();
+  const {
+    isLoading,
+    error,
+    data: dataResponse,
+  } = useFetchVercelDataCategory(
+    'country',
+    new Date(2024, 2, 10),
+    new Date(today.getFullYear(), today.getMonth(), today.getDate())
+  );
+
+  if (isLoading)
+    return (
+      <div>
+        <Skeleton className="w-full h-12" />
+      </div>
+    );
+
+  if (error || !dataResponse)
+    return <div>Error loading data for countries</div>;
+
+  // Split the data into two arrays
+  const firstRowData = dataResponse.data.slice(0, 3);
+  const secondRowData = dataResponse.data.slice(3);
+
+  return (
+    <div className="flex flex-col items-center justify-center py-4 bg-secondary/70">
+      <span
+        className={cn(
+          'text-lg uppercase font-bold text-warning text-center',
+          poppinsFont.className
+        )}
+      >
+        SPECIAL THANKS TO OUR USERS FROM ALL AROUND THE WORLD
+      </span>
+      {/* First row */}
+      <div className="flex flex-wrap gap-2 items-center justify-center p-2">
+        {firstRowData.map((data) => (
+          <span key={data.key} className="text-4xl">
+            {flag(data.key) ?? data.key}
+          </span>
+        ))}
+      </div>
+      {/* Second row */}
+      <div className="flex flex-wrap gap-2 items-center justify-center p-2">
+        {secondRowData.map((data) => (
+          <span key={data.key} className="text-4xl">
+            {flag(data.key) ?? data.key}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BotCountries;
