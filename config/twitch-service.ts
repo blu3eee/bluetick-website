54d1991f055cca8f93bbd303a96f41b7e186aa6e
bluesimp1102 <jackismyname7@gmail.com>
2024-03-27 13:02:55 -0700

    #26 instance to send request to microservice

---
+++ config/twitch-service.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosInstance } from 'axios';
+import { API_TOKEN } from './bluetick';
+
+export const TWITCH_SERVICE_URL = 'https://twitch.bluetick.khainguyen.dev';
+
+export const twitchInstance: AxiosInstance = axios.create({
+  baseURL: TWITCH_SERVICE_URL,
+  withCredentials: true,
+  headers: {
+    Authorization: `Web ${API_TOKEN}`,
+  },
+});
+
+export enum TWITCH_ROUTES {
+  STREAM_SUBS = '/streams',
+}
